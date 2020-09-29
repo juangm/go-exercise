e3c1482e670139de5851d4d21be574cf7c3df06a
juangm <juangarciamarinblog@gmail.com>
2020-09-29 14:13:52 +0200

    Finish go-htmlParser

---
+++ go-htmlParser/link/link.go
@@ -0,0 +1,77 @@
+package link
+
+import (
+	"io"
+	"io/ioutil"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+// Link represents a link (<a href="....">) in an HTML.
+type Link struct {
+	Href string
+	Text string
+}
+
+var r io.Reader
+
+// GetHTMLFile takes a path to a html file and return a string of the page content.
+func GetHTMLFile(path string) (string, error) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+// Parse will take in an HTML Document and will return a slice of links parsed from it.
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	nodes := linkNodes(doc)
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+	}
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = getTextLink(n)
+	return ret
+}
+
+func getTextLink(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += getTextLink(c)
+	}
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
+}
---
+++ go-htmlParser/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/juangm/go-exercises/go-htmlParser/link"
+)
+
+func main() {
+	file := flag.String("file", "./html_fixtures/test1.htm", "The Path to the HTML file to be parsed.")
+	flag.Parse()
+
+	htmlfile, err := link.GetHTMLFile(*file)
+	if err != nil {
+		panic(err)
+	}
+
+	r := strings.NewReader(htmlfile)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
