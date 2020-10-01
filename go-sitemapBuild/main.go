fcd5c17cc93138445294de59b31983ad883b3997
juangm <juangarciamarinblog@gmail.com>
2020-10-01 13:24:42 +0200

    Implement GET request

--- go-sitemapBuild/main.go
+++ go-sitemapBuild/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"net/http"
 )
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for.")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
-
 	// 1. GET the webpage
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 	// 2. Parse the links on the page (using previous package)
+
 	// 3. Build proper urls with our links
 	// 4. Filter out any links with a diff domain.
 	// 5. Find all pages (BFS)
