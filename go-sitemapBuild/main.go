33c76e8c80fdf25b8d36b4b42f8bef07031bcb7d
juangm <juangarciamarinblog@gmail.com>
2020-10-01 13:41:15 +0200

    Parsing and building links

--- go-sitemapBuild/main.go
+++ go-sitemapBuild/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/juangm/go-exercises/go-htmlParser/link"
 )
 
 func main() {
@@ -17,8 +22,30 @@ func main() {
 	defer resp.Body.Close()
 
 	// 2. Parse the links on the page (using previous package)
+	links, err := link.Parse(resp.Body)
+	if err != nil {
+		fmt.Printf("Some error happened when parsing the links!")
+	}
 
 	// 3. Build proper urls with our links
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+	base := baseURL.String()
+
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		default:
+			fmt.Println("Not taking the URL: ", l.Href)
+		}
+	}
 	// 4. Filter out any links with a diff domain.
 	// 5. Find all pages (BFS)
 	// 6. Print out XML
