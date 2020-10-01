0bc374aface97f3ebf1241af45e7298c202f2e78
juangm <juangarciamarinblog@gmail.com>
2020-10-01 13:57:26 +0200

    Finishing of filtering the links

--- go-sitemapBuild/main.go
+++ go-sitemapBuild/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -14,18 +15,40 @@ func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for.")
 	flag.Parse()
 
-	// 1. GET the webpage
-	resp, err := http.Get(*urlFlag)
+	pages := get(*urlFlag)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+
+	// 5. Find all pages (BFS)
+	// 6. Print out XML
+}
+
+func hrefs(body io.Reader, base string) []string {
+	links, err := link.Parse(body)
+	if err != nil {
+		fmt.Printf("Some error happened when parsing the links!")
+	}
+	var ret []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		default:
+			// fmt.Println("Not taking the URL: ", l.Href)
+		}
+	}
+	return ret
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-
-	// 2. Parse the links on the page (using previous package)
-	links, err := link.Parse(resp.Body)
-	if err != nil {
-		fmt.Printf("Some error happened when parsing the links!")
-	}
 
 	// 3. Build proper urls with our links
 	reqURL := resp.Request.URL
@@ -34,19 +57,16 @@ func main() {
 		Host:   reqURL.Host,
 	}
 	base := baseURL.String()
+	return filter(base, hrefs(resp.Body, base))
+}
 
-	var hrefs []string
-	for _, l := range links {
-		switch {
-		case strings.HasPrefix(l.Href, "/"):
-			hrefs = append(hrefs, base+l.Href)
-		case strings.HasPrefix(l.Href, "http"):
-			hrefs = append(hrefs, l.Href)
-		default:
-			fmt.Println("Not taking the URL: ", l.Href)
+func filter(base string, links []string) []string {
+	var ret []string
+	for _, link := range links {
+		// Only links with same base URL
+		if strings.HasPrefix(link, base) {
+			ret = append(ret, link)
 		}
 	}
-	// 4. Filter out any links with a diff domain.
-	// 5. Find all pages (BFS)
-	// 6. Print out XML
+	return ret
 }
